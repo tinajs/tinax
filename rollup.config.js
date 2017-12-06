cf39927d9b4aa7346ee52b7d80d24ea1e27c6305
yelo <zhihuzeye@gmail.com>
2017-12-06 18:19:51 +0800

    build: uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
+import uglify from 'rollup-plugin-uglify'
 import base from './rollup.conf.base.js'
 
 export default Object.assign({}, base, {
   output: {
-    file: 'dist/tinax.js',
+    file: 'dist/tinax.min.js',
     format: 'umd',
     name: 'Tinax',
   },
+  plugins: base.plugins.concat([
+    uglify(),
+  ]),
 })
